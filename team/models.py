76f8522f167d5721e0f74e12319ccc5a593421e9
Sergey <xxsmotur@gmail.com>
2020-01-28 15:46:06 +0500

    init

---
+++ team/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+from location_field.models.plain import PlainLocationField
+
+class Profile(User):
+    status = models.CharField(max_length=20)
+    description = models.CharField(max_length=500)
+    position = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    birthday = models.DateTimeField('birthday')
+    location = PlainLocationField(based_fields=['city'], zoom=7)
+
